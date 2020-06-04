8c110691d76c935b15c06a24379878ea93d91b64
Ray Chung <ray1422@ray-fish.me>
2020-06-04 21:47:03 +0800

    update something (unfinished)

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC
 
 import tensorflow as tf
@@ -44,6 +45,35 @@ def main():
     learning_rate = CustomSchedule(d_model)
     optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
 
+    def export(prenet_output, stops, post_output):
+        pass
+
+    def run_inference():
+        with open("sentences.txt") as f:
+            sentences = [x.strip().split(" ") for x in f.readlines()]
+        with open("Token_Index_Dict.CHEWING.json") as f:
+            token_dict = json.load(f)
+        for sentence in sentences:
+            _tokens = [token_dict[x] for x in sentence[0]]
+            _tokens = [token_dict['<S>']] + tokens + [token_dict['<E>']]
+            prenet_output, stops, post_output, attention_weights = model(_tokens)
+            export(prenet_output, stops, post_output)
+
+    def loss_function(_prenet_outputs, _stops, _postnet_outputs, _spe_length, _mels, _spes):
+        prenet_loss = tf.reduce_mean(tf.square(_prenet_outputs - _mels), axis=[-1])
+        postnet_loss = tf.reduce_mean(tf.square(_postnet_outputs - _spes), axis=[-1])
+
+        _stops = _stops[:, :, 0]
+        stop_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=_spe_length, logits=_stops)
+        stop_loss = tf.reduce_mean(stop_loss)
+        alpha, beta, gamma = 1., 1., 1.
+        losses = prenet_loss + postnet_loss
+        losses *= tf.sequence_mask(_spe_length, maxlen=losses.shape[1], dtype=tf.float32)
+        losses = tf.reduce_mean(tf.reduce_sum(losses, axis=-1) / tf.cast(_spe_length, dtype=tf.float32))
+        loss = losses + stop_loss
+
+        return loss, losses, stop_loss
+
     train_step_signature = [
         tf.TensorSpec(shape=(None, None), dtype=tf.int32),
         tf.TensorSpec(shape=(None, None, 80), dtype=tf.float32),
@@ -51,23 +81,22 @@ def main():
         tf.TensorSpec(shape=(None,), dtype=tf.int32),
     ]
 
+    def test_step(inputs, mels, spes, spe_lengths):
+        mels = tf.concat([tf.ones_like(mels[:, 0:1, :]), mels], axis=1)
+        mel_inp = mels[:, :-1, ...]
+        mel_real = mels[:, 1:, ...]
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inputs, mel_inp)
+
+        prenet_output, stops, post_output, attention_weights = model(inputs, mel_inp,
+                                                                     True,
+                                                                     enc_padding_mask,
+                                                                     combined_mask,
+                                                                     dec_padding_mask, training=False)
+        loss, losses, stop_loss = loss_function(prenet_output, stops, post_output, spe_lengths, mel_real, spes)
+        return loss, losses, stop_loss
+
     # @tf.function(input_signature=train_step_signature)
     def train_step(inputs, mels, spes, spe_lengths):
-
-        def loss_function(_prenet_outputs, _stops, _postnet_outputs, _spe_length, _mels, _spes):
-            prenet_loss = tf.reduce_mean(tf.square(_prenet_outputs - _mels), axis=[-1])
-            postnet_loss = tf.reduce_mean(tf.square(_postnet_outputs - _spes), axis=[-1])
-
-            _stops = _stops[:, :, 0]
-            stop_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=_spe_length, logits=_stops)
-            stop_loss = tf.reduce_mean(stop_loss)
-            alpha, beta, gamma = 1., 1., 1.
-            losses = prenet_loss + postnet_loss
-            losses *= tf.sequence_mask(_spe_length, maxlen=losses.shape[1], dtype=tf.float32)
-            losses = tf.reduce_mean(tf.reduce_sum(losses, axis=-1) / tf.cast(_spe_length, dtype=tf.float32))
-            loss = losses + stop_loss
-
-            return loss, losses, stop_loss
 
         mels = tf.concat([tf.ones_like(mels[:, 0:1, :]), mels], axis=1)
 
@@ -87,7 +116,6 @@ def main():
         gradients = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
         return loss, losses, stop_loss
-    
 
     train_dataset = get_pinyin_dataset("/work/ray200289/gst_tacotron_bert/TFRecords/train_chewing_bert_slim.tfrecords", 8, shuffle_size=30)
     valid_dataset = get_pinyin_dataset("/work/ray200289/gst_tacotron_bert/TFRecords/dev_chewing_bert_slim.tfrecords", 8)
@@ -99,6 +127,15 @@ def main():
 
             loss, losses, stop_loss = train_step(tokens, mels, spectrograms, spectrogram_length)
             print(f"steps {step}  loss: {loss:.5f}, losses: {losses:.5f}, stops: {stop_loss:.5f}")
+            if step % 1000 == 0:
+                run_inference()
+
+        val_losses = []
+        for step, (mels, spectrograms, spectrogram_length, tokens, token_length) in enumerate(valid_dataset):
+            loss = test_step(tokens, mels, spectrograms, spectrogram_length)
+            val_losses.append(loss)
+            print("validating.. step {step} \r", end="")
+        print(f"\n val_loss: {np.mean(val_losses): .5f}")
 
 
 if __name__ == '__main__':
